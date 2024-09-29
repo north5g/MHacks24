c5deccf289229141b2ca7a5d4b857d75f9839bdf
wongpiwat <wongpiwat.san@gmail.com>
2024-09-28 22:21:35 -0400

    code refactoring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,12 @@ import React, { useState } from "react";
 
 import { AppBar, Box, Button, List, TextField, Toolbar, Typography } from "@mui/material";
 
+import ArrowCircleUpRoundedIcon from "@mui/icons-material/ArrowCircleUpRounded";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import GrainIcon from "@mui/icons-material/Grain";
+
 type Message = {
-  user: string;
+  sender: string;
   text: string;
 };
 
@@ -14,35 +18,46 @@ export default function Home() {
   const [data, setData] = useState<Message[]>([]);
   const [text, setText] = useState<string>("");
 
-  const handleSend = () => {
+  const handleSend = (user: string) => {
     if (!text) return;
-    setData([...data, { user: "me", text: text }]);
+
+    const sender = user === "me" ? "me" : "ai";
+    const response = { sender: sender, text: text };
+
+    setData([...data, response]);
     setText("");
   };
 
   const handleChangeText = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (!event.target.value) return;
-    setText(event.target.value);
+    if (setText) setText(event.target.value);
+  };
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (event.key === "Enter") {
+      handleSend("ai");
+    }
   };
 
   const renderAIResponse = (response: Message) => {
     return (
-      <Box sx={{ display: "flex", alignItems: "baseline", backgroundColor: "#f4f4f4", borderRadius: 4, padding: 1 }}>
-        <Typography variant="h6" sx={{ color: "black" }}>{response.user}: </Typography>
+      <Box
+        sx={{ display: "flex", alignItems: "center", backgroundColor: "#f4f4f4", borderRadius: 4, padding: 1, gap: 1 }}>
+        <GrainIcon />
         <Typography variant="body1" sx={{ color: "black" }}>{response.text}</Typography>
       </Box>);
   };
 
   const renderUserMessage = (message: Message) => {
     return (
-      <Box sx={{ display: "flex", alignItems: "baseline", backgroundColor: "#f4f4f4", borderRadius: 4, padding: 1 }}>
-        <Typography variant="h6" sx={{ color: "black" }}>{message.user}: </Typography>
+      <Box
+        sx={{ display: "flex", alignItems: "center", backgroundColor: "#f4f4f4", borderRadius: 4, padding: 1, gap: 1 }}>
+        <AccountCircleIcon />
         <Typography variant="body1" sx={{ color: "black" }}>{message.text}</Typography>
       </Box>);
   };
 
   return (
-    <Box sx={{ flexGrow: 1, backgroundColor: "#424242" }}>
+    <Box sx={{ flexGrow: 1, backgroundColor: "#212121" }}>
       <AppBar position="static">
         <Toolbar sx={{ gap: 1, backgroundColor: "#aaaaaa" }}>
           <Typography variant="h6" component="div" sx={{ flexGrow: 0 }}>
@@ -54,21 +69,20 @@ export default function Home() {
           <Typography variant="h6" component="div">MHacks 24</Typography>
         </Toolbar>
       </AppBar>
-      <Box sx={{ height: "80vh", width: "100%" }}>
-
-        {/* display chat messages */}
-        <List sx={{ padding: 1, overflowY: "scroll" }}>
+      <Box sx={{ height: "80vh", width: "100%", overflowY: "scroll" }}>
+        {/* display messages */}
+        <List sx={{ padding: 2 }}>
           {data.map((message, index) => (
             <Box key={index} sx={{ display: "flex", paddingBottom: 1 }}>
-              {message.user === "me" ? renderUserMessage(message) : renderAIResponse(message)}
+              {message.sender === "me" ? renderUserMessage(message) : renderAIResponse(message)}
             </Box>
           ))}
         </List>
       </Box>
 
-      <Box sx={{ display: "flex", padding: 2, gap: 2, backgroundColor: "#aaaaaa" }}>
-        <TextField value={text} variant="outlined" fullWidth onChange={handleChangeText} />
-        <Button variant="outlined" onClick={handleSend}>Send</Button>
+      <Box sx={{ display: "flex", padding: 2, gap: 2, backgroundColor: "#e3e3e3", boxShadow: 3 }}>
+        <TextField value={text} variant="outlined" fullWidth onChange={handleChangeText} onKeyPress={handleKeyPress} />
+        <Button variant="outlined" onClick={() => handleSend("me")}><ArrowCircleUpRoundedIcon /></Button>
       </Box>
 
     </Box>
