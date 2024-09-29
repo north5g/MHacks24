de6a95d54f3f64467c522cc4f67016da78640438
wongpiwat <wongpiwat.san@gmail.com>
2024-09-29 10:01:38 -0400

    implement API call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,25 @@
 
 import React, { useEffect, useRef, useState } from "react";
 
-import { AppBar, Box, Button, List, TextField, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  List,
+  TextField,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 
 import ArrowCircleUpRoundedIcon from "@mui/icons-material/ArrowCircleUpRounded";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import GrainIcon from "@mui/icons-material/Grain";
+import { send } from "@/services/ai.service";
 
 type Message = {
   sender: string;
   text: string;
 };
-
 
 export default function Home() {
   const [messages, setMessages] = useState<Message[]>([]);
@@ -28,14 +36,26 @@ export default function Home() {
     scrollToBottom();
   }, [messages]);
 
-  const handleSend = (user: string) => {
+  const handleSend = async (user: string) => {
     if (!text) return;
 
+    // Add user message to the list
     const sender = user === "me" ? "me" : "ai";
     const response = { sender: sender, text: text };
-
     setMessages([...messages, response]);
     setText("");
+
+    // Send message to the AI
+    try {
+      const response = await send(text);
+      console.log("[DEBUG] response", response);
+
+      // Add AI response to the list
+      const aiResponse = { sender: "ai", text: response.text };
+      setMessages([...messages, aiResponse]);
+    } catch (error) {
+      console.error(error as string);
+    }
   };
 
   const handleChangeText = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -51,19 +71,41 @@ export default function Home() {
   const renderAIResponse = (response: Message) => {
     return (
       <Box
-        sx={{ display: "flex", alignItems: "center", backgroundColor: "#f4f4f4", borderRadius: 4, padding: 1, gap: 1 }}>
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          backgroundColor: "#f4f4f4",
+          borderRadius: 4,
+          padding: 1,
+          gap: 1,
+        }}
+      >
         <GrainIcon />
-        <Typography variant="body1" sx={{ color: "black" }}>{response.text}</Typography>
-      </Box>);
+        <Typography variant="body1" sx={{ color: "black" }}>
+          {response.text}
+        </Typography>
+      </Box>
+    );
   };
 
   const renderUserMessage = (message: Message) => {
     return (
       <Box
-        sx={{ display: "flex", alignItems: "center", backgroundColor: "#f4f4f4", borderRadius: 4, padding: 1, gap: 1 }}>
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          backgroundColor: "#f4f4f4",
+          borderRadius: 4,
+          padding: 1,
+          gap: 1,
+        }}
+      >
         <AccountCircleIcon />
-        <Typography variant="body1" sx={{ color: "black" }}>{message.text}</Typography>
-      </Box>);
+        <Typography variant="body1" sx={{ color: "black" }}>
+          {message.text}
+        </Typography>
+      </Box>
+    );
   };
 
   return (
@@ -76,7 +118,9 @@ export default function Home() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             DevBuddyAI
           </Typography>
-          <Typography variant="h6" component="div">MHacks 24</Typography>
+          <Typography variant="h6" component="div">
+            MHacks 24
+          </Typography>
         </Toolbar>
       </AppBar>
       <Box sx={{ height: "80vh", width: "100%", overflowY: "scroll" }}>
@@ -84,18 +128,35 @@ export default function Home() {
         <List sx={{ padding: 2 }}>
           {messages.map((message, index) => (
             <Box key={index} sx={{ display: "flex", paddingBottom: 1 }}>
-              {message.sender === "me" ? renderUserMessage(message) : renderAIResponse(message)}
+              {message.sender === "me"
+                ? renderUserMessage(message)
+                : renderAIResponse(message)}
             </Box>
           ))}
         </List>
         <div ref={messagesEndRef} />
       </Box>
 
-      <Box sx={{ display: "flex", padding: 2, gap: 2, backgroundColor: "#e3e3e3", boxShadow: 3 }}>
-        <TextField value={text} variant="outlined" fullWidth onChange={handleChangeText} onKeyPress={handleKeyPress} />
-        <Button variant="outlined" onClick={() => handleSend("me")}><ArrowCircleUpRoundedIcon /></Button>
+      <Box
+        sx={{
+          display: "flex",
+          padding: 2,
+          gap: 2,
+          backgroundColor: "#e3e3e3",
+          boxShadow: 3,
+        }}
+      >
+        <TextField
+          value={text}
+          variant="outlined"
+          fullWidth
+          onChange={handleChangeText}
+          onKeyPress={handleKeyPress}
+        />
+        <Button variant="outlined" onClick={() => handleSend("me")}>
+          <ArrowCircleUpRoundedIcon />
+        </Button>
       </Box>
-
     </Box>
   );
 }
--- src/services/ai.service.ts
+++ src/services/ai.service.ts
@@ -1,1 +1,19 @@
+import axios from "axios";
 
+const API_ENDPOINT = "http://localhost:3000";
+
+export const send = async (text: string): Promise<any> => {
+  const options = {
+    url: `${API_ENDPOINT}/`,
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json;charset=UTF-8",
+    },
+    params: {
+      text,
+    },
+  };
+  const response = await axios(options);
+  return response.data;
+};
